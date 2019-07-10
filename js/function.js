b6553319c725ff4bd5cfdadea441fcd274e89f7a
rizkiagustina <52686541+rizkiagustina@users.noreply.github.com>
2019-07-10 22:02:46 +0700

    Add files via upload

--- js/function.js
+++ js/function.js
@@ -1,1 +1,23 @@
+/* Demo purposes only */
+$(".hover").mouseleave(
+  function () {
+    $(this).removeClass("hover");
+  }
+);
+
+
+
+/* Demo purposes only */
+var snippet = [].slice.call(document.querySelectorAll('.hover'));
+if (snippet.length) {
+  snippet.forEach(function (snippet) {
+    snippet.addEventListener('mouseout', function (event) {
+      if (event.target.parentNode.tagName === 'figure') {
+        event.target.parentNode.classList.remove('hover')
+      } else {
+        event.target.parentNode.classList.remove('hover')
+      }
+    });
+  });
+}
 
